4c57a9be545c34396aa8321bde4a85f5e2fe3c68
Shatakashi <93199129+Shatakashi@users.noreply.github.com>
2021-10-26 19:34:15 +0530

    updated

--- code.cpp
+++ code.cpp
@@ -1,4 +1,5 @@
 // C++ program for implementation of Bubble sort
+//Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 #include <bits/stdc++.h>
 using namespace std;
 
