29802122d2a4a1e37e1ce1d0fa6c6aa04a965cc2
Priyanka Hiranandani <priyankahiranandani@gmail.com>
2021-10-31 06:46:11 +0530

    Update Bubble short (java).java

--- Bubble short (java).java
+++ Bubble short (java).java
@@ -2,17 +2,19 @@ public class BubbleSortExample {
     static void bubbleSort(int[] arr) {  
         int n = arr.length;  
         int temp = 0;  
+        int flag = 0;
          for(int i=0; i < n; i++){  
                  for(int j=1; j < (n-i); j++){  
                           if(arr[j-1] > arr[j]){  
                                  //swap elements  
                                  temp = arr[j-1];  
                                  arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                         }  
-                          
+                                 arr[j] = temp;
+                                 flag = 1;
+                         }                          
                  }  
-         }  
+              if(flag == 0) break;
+         }   
   
     }  
     public static void main(String[] args) {  
